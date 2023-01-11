767d3517799be7051ac5e0d825df6dfa35141561
iago sauer <iago_ortiz@hotmail.com>
2023-01-10 22:27:33 -0400

    House 10/01/2023

--- lib/Repositories/documentos_repository.dart
+++ lib/Repositories/documentos_repository.dart
@@ -6,9 +6,9 @@ import 'package:portalsped/Models/documentos_model.dart';
 
 class DocumentosRepository {
   Dio dio = Dio();
-  Future<List<DocumentosModel>> fetchClientes(String escritorio) async {
+  Future<List<DocumentosModel>> fetchDocumentos(String pai) async {
     try {
-      final response = await dio.get('${Valor.baseUrl}/$escritorio');
+      final response = await dio.get('${Valor.baseUrl}/$pai');
       final lista = JsonDecoder(response.data) as List;
       return lista.map((e) => DocumentosModel.fromMap(e)).toList();
     } catch (e) {

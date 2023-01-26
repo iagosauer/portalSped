b2f0a3cf9a079ccba1a259474e3acbd312e109ea
Iago Sauer <iago_ortiz@hotmail.com>
2023-01-26 11:04:29 -0400

    Foi colocado os icones do Portal

--- lib/Classes/Valores.dart
+++ lib/Classes/Valores.dart
@@ -1,5 +1,5 @@
 class Valor {
-  static String baseUrl = 'http://192.168.0.106:8080';
+  static String baseUrl = 'http://portalsoft.sytes.net:8080';
   static String baseUrlDownload = 'http://192.168.0.106:8000';
 
 }
